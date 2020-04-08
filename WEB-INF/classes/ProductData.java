39842af50e9e828e0d7ba2217d672dc04169e793
JuliaMtzD <62984594+JuliaMtzD@users.noreply.github.com>
2020-04-08 12:35:06 +0200

    Update ProductData.java

--- WEB-INF/classes/ProductData.java
+++ WEB-INF/classes/ProductData.java
@@ -52,9 +52,8 @@ public class ProductData {
 
         Vector<ProductData> vec = new Vector<ProductData>();
 
-        String sql = "Select ProductId, ProductName, Products.SupplierId as SupplierId, CompanyName, UnitPrice FROM Products, Suppliers";
+        String sql = "SELECT Products.ProductID, Products.ProductName, Products.SellingPrice, Products.Category FROM Products";
 
-        sql += " WHERE Products.SupplierId = Suppliers.SupplierId";
 
         System.out.println("getProductList: " + sql);
 
@@ -68,15 +67,13 @@ public class ProductData {
 
                 ProductData product = new ProductData(
 
-                    result.getString("ProductId"),
+                    Integer.parseInt(result.getString("ProductId")),
 
                     result.getString("ProductName"),
 
-                    Integer.parseInt(result.getString("SupplierId")),
-
-                    result.getString("CompanyName"),
-
-                    Float.parseFloat(result.getString("UnitPrice"))
+                    Integer.parseInt(result.getString("SellingPrice")),
+					
+					Integer.parseInt(result.getString("Category")),
 
                 );
 
@@ -96,257 +93,5 @@ public class ProductData {
 
     }
 
-    public static Vector<ProductData> getCategoryProductList(Connection connection, String id) {
 
-        Vector<ProductData> vec = new Vector<ProductData>();
-
-        String sql = "Select ProductId, ProductName, Products.SupplierId as SupplierId, CompanyName, UnitPrice FROM Products, Suppliers";
-
-        sql += " WHERE Products.SupplierId = Suppliers.SupplierId AND CategoryID=?";
-
-        System.out.println("getProductList: " + sql);
-
-        try {
-
-            PreparedStatement pstmt=connection.prepareStatement(sql);
-
-            pstmt.setInt(1, Integer.parseInt(id));
-
-            ResultSet result = pstmt.executeQuery();
-
-            while(result.next()) {
-
-                ProductData product = new ProductData(
-
-                    result.getString("ProductId"),
-
-                    result.getString("ProductName"),
-
-                    Integer.parseInt(result.getString("SupplierId")),
-
-                    result.getString("CompanyName"),
-
-                    Float.parseFloat(result.getString("UnitPrice"))
-
-                );
-
-                vec.addElement(product);
-
-            }
-
-        } catch(SQLException e) {
-
-            e.printStackTrace();
-
-            System.out.println("Error in getProductList: " + sql + " Exception: " + e);
-
-        }
-
-        return vec;
-
-    }
-	
-
-    public static ProductData getProduct(Connection connection, String id) {
-
-        String sql = "Select ProductId, ProductName, SupplierId, UnitPrice FROM Products";
-
-        sql += " WHERE ProductId=?";
-
-        System.out.println("getProduct: " + sql);
-
-        ProductData product = null;;
-
-        try {
-
-            PreparedStatement pstmt = connection.prepareStatement(sql);
-
-            pstmt.setString(1, id);
-
-            ResultSet result = pstmt.executeQuery();
-
-            if(result.next()) {
-
-                product = new ProductData(
-
-                    result.getString("ProductId"),
-
-                    result.getString("ProductName"),
-
-                    Integer.parseInt(result.getString("SupplierId")),
-
-                    null,
-
-                    Float.parseFloat(result.getString("UnitPrice"))
-
-                );
-
-            }
-
-            result.close();
-
-            pstmt.close();
-
-        } catch(SQLException e) {
-
-            e.printStackTrace();
-
-            System.out.println("Error in getProduct: " + sql + " Exception: " + e);
-
-        }
-
-        return product;
-
-    }
-
-
-    public static ProductData getProduct2(Connection connection, String id) {
-
-        String sql = "Select * FROM Products";
-
-        sql += " WHERE ProductId=?";
-
-        System.out.println("getProduct: " + sql);
-
-        ProductData product = null;;
-
-        try {
-
-            PreparedStatement pstmt = connection.prepareStatement(sql);
-
-            pstmt.setString(1, id);
-
-            ResultSet result = pstmt.executeQuery();
-
-            if(result.next()) {
-
-                product = new ProductData(
-
-                    result.getString("ProductId"),
-
-                    result.getString("ProductName"),
-
-                    Integer.parseInt(result.getString("SupplierId")),
-
-                    null,
-
-                    Float.parseFloat(result.getString("UnitPrice")),
-
-					Integer.parseInt(result.getString("CategoryId")),
-
-					Integer.parseInt(result.getString("QuantityPerUnit")), 
-
-					Integer.parseInt(result.getString("UnitsInStock")),
-
-					Integer.parseInt(result.getString("UnitsOnOrder"))
-
-
-                );
-            }
-
-            result.close();
-
-            pstmt.close();
-
-        } catch(SQLException e) {
-
-            e.printStackTrace();
-
-            System.out.println("Error in getProduct: " + sql + " Exception: " + e);
-
-        }
-
-        return product;
-
-    }
-
-
-	public static int updateProduct(Connection connection, ProductData product) {
-
-        String sql ="UPDATE Products "
-
-            + "SET ProductName = ?, SupplierId = ?, UnitPrice = ?"
-
-            + " WHERE ProductId = ?";
-
-        System.out.println("updateProduct: " + sql);
-
-        int n = 0;
-
-        try {
-
-            PreparedStatement stmtUpdate= connection.prepareStatement(sql);
-
-            stmtUpdate.setString(1,product.productName);
-
-            stmtUpdate.setInt(2,product.supplierId);
-
-            stmtUpdate.setFloat(3,product.unitPrice);
-
-            stmtUpdate.setString(4,product.productId);
-
-            n = stmtUpdate.executeUpdate();
-
-            stmtUpdate.close();
-
-        } catch(SQLException e) {
-
-            e.printStackTrace();
-
-            System.out.println("Error in updateProduct: " + sql + " Exception: " + e);
-
-        }
-
-        return n;
-
-    }
-
-		public static int updateProduct2(Connection connection, ProductData product) {
-
-        String sql ="UPDATE Products "
-
-            + "SET ProductName = ?, SupplierId = ?, UnitPrice = ?, CategoryId=?, QuantityPerUnit=?,UnitsInStock=?, UnitsOnOrder=?"
-
-            + " WHERE ProductId = ?";
-
-        System.out.println("updateProduct: " + sql);
-
-        int n = 0;
-
-        try {
-
-            PreparedStatement stmtUpdate= connection.prepareStatement(sql);
-
-            stmtUpdate.setString(1,product.productName);
-
-            stmtUpdate.setInt(2,product.supplierId);
-
-            stmtUpdate.setFloat(3,product.unitPrice);
-
-			stmtUpdate.setInt(4,product.categoryId);
-
-            stmtUpdate.setInt(5,product.quantityPerUnit);
-
-            stmtUpdate.setInt(6,product.unitsInStock);
-
-            stmtUpdate.setInt(7,product.unitsOnOrder);
-
-			stmtUpdate.setString(8,product.productId);
-
-
-			n = stmtUpdate.executeUpdate();
-
-            stmtUpdate.close();
-
-        } catch(SQLException e) {
-
-            e.printStackTrace();
-
-            System.out.println("Error in updateProduct: " + sql + " Exception: " + e);
-
-        }
-
-        return n;
-
-    }
 }
