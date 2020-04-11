6111adb9f097ddac47d3e9dd1b3e9374bc0d7780
JuliaMtzD <62984594+JuliaMtzD@users.noreply.github.com>
2020-04-11 20:38:20 +0200

    Update ClientMyAccount.java

--- WEB-INF/classes/ClientMyAccount.java
+++ WEB-INF/classes/ClientMyAccount.java
@@ -20,24 +20,75 @@ public class ClientMyAccount extends HttpServlet {
         
 
         toClient.println(Utils.header("My Account"));
-		toClient.println("<table border='1'>");
+
 		
-		Vector<ClientData2> clientList;
-		clientList = ClientData2.getClientList(connection);
+		String name="Mauris Foundation"; 
 		
-		for(int i=0; i< clientList.size(); i++){
-                ClientData2 client = clientList.elementAt(i);
-                toClient.println("<tr>");
-                toClient.println("<td>" + client.clientId + " </td>");
-                toClient.println("<td>" + client.clientName + " </td>");
-                toClient.println("<td>" + client.clientadress + " </td>");
-                toClient.println("<td>" + client.creditcard + " </td>");
-                toClient.println("<td>" + client.cleintemail + " </td>");
-                toClient.println("<td>" + client.clientpassword + " </td>");
-                toClient.println("<td>" + client.clienttelephone + " </td>");
-                toClient.println("</tr>");
+		Vector<ClientData2> clientdata;
+		clientdata= ClientData2.getClient(connection,name);
+		
+		for(int i=0; i< clientdata.size(); i++){
+                ClientData2 client = clientdata.elementAt(i);
+
+
+				toClient.println("<form action='ClientMyAccount.html' target='_blank'>");
+				toClient.println("<div class='w3-container w3-dark-grey w3-center w3-padding-32 w3-padding-large w3-text-align:left' >");
+				toClient.println("<h4 class='w3-center'><b> Personal Information </b></h4>");
+				
+				toClient.println("<div class='w3-section' >");
+				toClient.println("<label for='ClientID'> Username </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientID' id='ClientID' readonly value='"+name+"' >");
+				toClient.println("</div>");
+				
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientName'> Contact Name </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientName' id='ClientName'  placeholder='Your name...'>");
+				toClient.println("</div>");
+				
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientLastName'> Contact Last Name </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientLastName' id='ClientLastName'  placeholder='Your lastname...'>");
+				toClient.println("</div>");
+				
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientPassword'> Password </label>");
+				toClient.println("<input class='w3-input w3-border' type='password' name='ClientPassword' id='ClientPassword'  placeholder='"+client.clientpassword+"'>");
+				toClient.println("</div>");
+				toClient.println("</div>");
+				
+				toClient.println("<div class='w3-container w3-grey w3-center w3-padding-32 w3-padding-large w3-text-align:left' >");
+				toClient.println("<h4 class='w3-center'><b> Contact Information </b></h4>");
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientPhone'> Phone Number </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientPhone' id='ClientPhone'  placeholder='" + client.clienttelephone + "'>");
+				toClient.println("</div>");
+				
+				
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientEmail'> Email </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientEmail' id='ClientEmail'  placeholder='" + client.cleintemail + " '>");
+				toClient.println("</div>");
+				
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientAddress'> Address </label>");
+				toClient.println("<input class='w3-input w3-border' type='text' name='ClientAddress' id='ClientAddress'  placeholder='" + client.clientadress + "'>");
+				toClient.println("</div>");
+				toClient.println("</div>");
+			 
+				toClient.println("<div class='w3-container w3-dark-grey w3-center w3-padding-32 w3-padding-large w3-text-align:left'>");
+				toClient.println("<h4 class='w3-center'><b> Payment Information </b></h4>");
+				toClient.println("<div class='w3-section'>");
+				toClient.println("<label for='ClientCard'> Credit card </label>");
+				toClient.println("<input class='w3-input w3-border' type='password' name='ClientCard' id='ClientCard'  placeholder='" + client.creditcard + "'>");
+				toClient.println("</div>");
+				toClient.println("</div>");
+				
+				
+				toClient.println("<input type='submit' value='Save Changes' class='w3-button w3-block w3-yellow w3-margin-bottom' >");
+				
+				toClient.println("</form>");
         }
-		toClient.println("</table>");
+
         toClient.println(Utils.footer("My Account"));
         toClient.close();
     }
