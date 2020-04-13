87db7c4d9730e751e63b7465584f922895bca029
marmunozpons <a905348@alumni.unav.es>
2020-04-13 19:48:15 +0200

    Create WorkerDelete.java

---
+++ WEB-INF/classes/WorkerDelete.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.Connection;
+
+@SuppressWarnings("serial")
+public class WorkerDelete extends HttpServlet {
+    Connection connection;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        connection = ConnectionUtils.getConnection(config);
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException  {
+        res.setContentType("text/html");
+        String idStr = req.getParameter("WorkerID");
+		
+		
+		      
+		WorkerData worker = new WorkerData(
+		
+                    Integer.parseInt(req.getParameter("WorkerID")),
+                    req.getParameter("WorkerName"),
+                    req.getParameter("WorkerLastName"),
+                    req.getParameter("WorkerPassword"),
+                    req.getParameter("WorkerEmail"), 
+                    req.getParameter("WorkerDepartment")        
+                );
+				
+		
+		 int n2 = WorkerData.deleteWorker(connection, worker);
+
+		
+		//---------------------------------------------
+ res.sendRedirect("index.html");
+    }
+}
